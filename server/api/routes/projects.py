c4eb14d29e55baa8d786c0c22c3bfd89e2fa9f1c
Tim Repke <repke@mcc-berlin.net>
2022-12-15 18:32:11 +0100

    add endpoint for creating project (slightly redundant to PUT /project/info)

--- server/api/routes/projects.py
+++ server/api/routes/projects.py
@@ -1,4 +1,8 @@
+import uuid
+from typing import TYPE_CHECKING
+
 from fastapi import APIRouter, Depends
+from nacsos_data.db.schemas import Project
 
 from nacsos_data.models.users import UserModel
 from nacsos_data.models.projects import ProjectModel
@@ -6,8 +10,11 @@ from nacsos_data.db.crud.projects import read_all_projects, read_all_projects_fo
 
 from server.api.errors import MissingInformationError
 from server.data import db_engine
-from server.util.security import get_current_active_user
+from server.util.security import get_current_active_user, get_current_active_superuser
 from server.util.logging import get_logger
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 logger = get_logger('nacsos.api.route.projects')
 router = APIRouter()
@@ -31,3 +38,14 @@ async def get_all_projects(current_user: UserModel = Depends(get_current_active_
         raise MissingInformationError('`current_user` has no `user_id`, which points to a serious issue in the system!')
 
     return await read_all_projects_for_user(current_user.user_id, engine=db_engine)
+
+
+@router.put('/create', response_model=str)
+async def create_project(project: ProjectModel,
+                         superuser: UserModel = Depends(get_current_active_superuser)) -> str:
+    async with db_engine.session() as session:  # type: AsyncSession
+        if project.project_id is None:
+            project.project_id = str(uuid.uuid4())
+        session.add(Project(**project.dict()))
+        await session.commit()
+    return project.project_id

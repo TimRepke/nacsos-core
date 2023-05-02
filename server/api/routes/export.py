c969aaa886dca8c31cf4c27e29875f22c6c3d80e
Tim Repke <repke@mcc-berlin.net>
2023-05-02 20:01:56 +0200

    add export route wip

--- server/api/routes/export.py
+++ server/api/routes/export.py
@@ -1,7 +1,5 @@
 import json
 from typing import TYPE_CHECKING
-import tempfile
-import csv
 
 from fastapi import APIRouter, Depends, Query
 from fastapi.responses import PlainTextResponse
@@ -9,10 +7,8 @@ from nacsos_data.db.schemas import AcademicItem, Project, ItemType, TwitterItem
 from nacsos_data.util.annotations.export import prepare_export_table, LabelSelector
 
 from server.util.security import \
-    UserPermissionChecker, \
-    InsufficientPermissions
+    UserPermissionChecker
 
-from nacsos_data.util.annotations import export as anno_export
 from nacsos_data.util.auth import UserPermissions
 
 from server.data import db_engine
@@ -25,30 +21,34 @@ router = APIRouter()
 
 @router.get('/annotations/csv', response_class=PlainTextResponse)
 async def get_annotations_csv(labels: list[LabelSelector],
-                                 bot_annotation_metadata_ids: list[str] | None = Query(default=None),
-                                 assignment_scope_ids: list[str] | None = Query(default=None),
-                                 user_ids: list[str] | None = Query(default=None),
-                                 item_fields: list[str] | None = Query(default=None),
-                                 permissions: UserPermissions = Depends(UserPermissionChecker('annotations_read'))) \
+                              bot_annotation_metadata_ids: list[str] | None = Query(default=None),
+                              assignment_scope_ids: list[str] | None = Query(default=None),
+                              user_ids: list[str] | None = Query(default=None),
+                              item_fields: list[str] | None = Query(default=None),
+                              permissions: UserPermissions = Depends(UserPermissionChecker('annotations_read'))) \
         -> str:
     async with db_engine.session() as session:  # type: AsyncSession
         project = await session.get(Project, permissions.permissions.project_id)
-        if project.type == ItemType.academic:
-            item_type = AcademicItem
-        elif project.type == ItemType.twitter:
-            item_type = TwitterItem
 
-    labels = await prepare_export_table(bot_annotation_metadata_ids=bot_annotation_metadata_ids,
+        if project is None:
+            raise RuntimeError('This should not happen!')
+
+        if project.type == ItemType.academic:
+            item_type = AcademicItem  # type: ignore[assignment]
+        elif project.type == ItemType.twitter:
+            item_type = TwitterItem  # type: ignore[assignment]
+
+    result = await prepare_export_table(bot_annotation_metadata_ids=bot_annotation_metadata_ids,
                                         assignment_scope_ids=assignment_scope_ids,
                                         user_ids=user_ids,
                                         labels=labels,
-                                        item_fields=[AcademicItem.text, AcademicItem.title],
-                                        item_type=item_type,
+                                        item_fields=[AcademicItem.text, AcademicItem.title],  # type: ignore[list-item]
+                                        item_type=item_type,  # type: ignore[type-var]
                                         db_engine=db_engine)
 
     ret = ''
-    for l in labels[:10]:
-        ret += json.dumps(l) + '\n'
+    for lab in result[:10]:
+        ret += json.dumps(lab) + '\n'
     return ret
 
     # with tempfile.TemporaryFile() as fp:

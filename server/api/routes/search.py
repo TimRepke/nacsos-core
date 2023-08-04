b68b4031b9d5d1cf6bc3e088b111d6dff469d25b
Tim Repke <repke@mcc-berlin.net>
2023-08-04 11:06:52 +0200

    add oa search route

--- server/api/routes/search.py
+++ server/api/routes/search.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter, Depends
-from pydantic import BaseModel
+from typing import Literal
+
 import httpx
+from pydantic import BaseModel
+from fastapi import APIRouter, Depends
+
+from nacsos_data.util.academic.openalex import query_async, SearchResult
 
 from server.util.security import UserPermissionChecker, UserPermissions
 from server.util.logging import get_logger
@@ -18,11 +22,23 @@ class TermStats(BaseModel):
     ttf: int
 
 
-@router.get('/openalex', response_model=str)
-async def search_openalex(import_id: str,
-                          permissions: UserPermissions = Depends(UserPermissionChecker('search_oa'))):
-    # u = settings.OA_SOLR
-    pass
+@router.get('/openalex', response_model=SearchResult)
+async def search_openalex(query: str,
+                          limit: int = 20,
+                          field: Literal['title', 'abstract', 'title_abstract'] = 'title_abstract',
+                          histogram: bool = False,
+                          op: Literal['OR', 'AND'] = 'AND',
+                          histogram_from: int = 1990,
+                          histogram_to: int = 2024,
+                          permissions: UserPermissions = Depends(UserPermissionChecker('search_oa'))) -> SearchResult:
+    return await query_async(query=query,
+                             openalex_endpoint=f'{settings.OA_SOLR}/select',
+                             histogram=histogram,
+                             histogram_to=histogram_to,
+                             histogram_from=histogram_from,
+                             op=op,
+                             field=field,
+                             limit=limit)
 
 
 @router.post('/terms', response_model=list[TermStats])

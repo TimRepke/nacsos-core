cebec9e4223213701b1b713c43df54ead332e83b
Tim Repke <repke@mcc-berlin.net>
2023-10-12 15:35:19 +0200

    fix build

--- server/api/routes/project/items.py
+++ server/api/routes/project/items.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Query
 from nacsos_data.db.crud.items.lexis_nexis import read_lexis_paged_for_project
-from nacsos_data.db.schemas import Project, ItemTypeLiteral, GenericItem, AcademicItem, ItemType, Item, LexisNexisItem, \
-    LexisNexisItemSource
+from nacsos_data.db.schemas import Project, ItemTypeLiteral, GenericItem, AcademicItem, ItemType, Item, LexisNexisItem
 
 from nacsos_data.models.items import AnyItemModel, GenericItemModel, AcademicItemModel, AnyItemModelList, \
     LexisNexisItemModel
@@ -16,7 +15,7 @@ from nacsos_data.db.crud.items.twitter import \
     read_all_twitter_items_for_project_paged, \
     read_twitter_item_by_item_id, \
     import_tweet
-from sqlalchemy import select, func
+from sqlalchemy import select
 
 from server.api.errors import ItemNotFoundError
 from server.data import db_engine

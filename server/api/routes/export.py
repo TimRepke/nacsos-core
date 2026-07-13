01ce17d715230a67cffbc05a75acb1cd71defa3d
Gizem Özcan <gizemoe@pik-potsdam.de>
2026-07-13 12:25:09 +0200

    add rispy exports

--- server/api/routes/export.py
+++ server/api/routes/export.py
@@ -1,7 +1,7 @@
 import csv
 import os
 import tempfile
-# import rispy
+import rispy
 import xlsxwriter
 import json
 import uuid
@@ -281,63 +281,79 @@ async def get_annotations_excel(
     )
 
 
-# @router.post('/annotations/ris', response_class=CFR)
-# async def get_annotations_ris(
-#     query: ExportRequest,
-#     permissions: UserPermissions = Depends(UserPermissionChecker('annotations_read')),
-# ) -> FileResponse:
-#     labels = query.labels
-#     result = await prepare_export_table(
-#         bot_annotation_metadata_ids=query.bot_annotation_metadata_ids,
-#         assignment_scope_ids=query.assignment_scope_ids,
-#         user_ids=query.user_ids,
-#         project_id=permissions.permissions.project_id,
-#         labels=labels,
-#         nql_filter=query.nql_filter,
-#         ignore_repeat=query.ignore_repeat,
-#         ignore_hierarchy=query.ignore_hierarchy,
-#         db_engine=db_engine,
-#     )
+def get_author_names(authors: Any) -> list[str]:
+    if not isinstance(authors, list) or not authors:
+        return []
+    
+    first = authors[0]
+    # academic item
+    if isinstance(first, dict) and 'name' in first.keys():
+        return [a.get('name') for a in authors]
+    # lexis item
+    if isinstance(first, str):
+        return authors
+    return []
 
-#     with tempfile.NamedTemporaryFile(suffix='.ris', mode='w', delete=False) as bibliography_file:
-#         rispy.dump(
-#             [
-#                 {  # In prod academic_items; up to 2.5M out of 8M missing for these columns; so to make it error prone, default to empty string
-#                     'abstract': row.get('text'),
-#                     'title': row.get('title'),
-#                     'doi': f'https://doi.org/{row.get("doi")}',
-#                     'custom1': row.get('openalex_id'),
-#                     'custom2': str(row.get('item_id')),
-#                     'year': row.get('publication_year'),
-#                     'journal_name': row.get('source'),
-#                     'authors': [author.get('name') for author in row.get('authors')] if row.get('authors') else [],
-#                     'keywords': (row.get('keywords') if row.get('keywords') else [])
-#                     + [f'{l.key}|{li}' for l in labels if l.options_int is not None for li in l.options_int if row[f'{l.key}|{li}'] == 1]
-#                     + [f'{l.key}|{li}' for l in labels if l.options_multi is not None for li in l.options_multi if row[f'{l.key}|{li}'] == 1]
-#                     + [f'{l.key}|{li}' for l in labels if l.options_bool is not None for li in [0, 1] if row[f'{l.key}|{li}'] == 1],
-#                     'label': [f'{l.key}|{li}' for l in labels if l.options_int is not None for li in l.options_int if row[f'{l.key}|{li}'] == 1]
-#                     + [f'{l.key}|{li}' for l in labels if l.options_multi is not None for li in l.options_multi if row[f'{l.key}|{li}'] == 1]
-#                     + [f'{l.key}|{li}' for l in labels if l.options_bool is not None for li in [0, 1] if row[f'{l.key}|{li}'] == 1],
-#                     'notes': [
-#                         f'openalex: {row.get("openalex_id")}\n'
-#                         f'nacsos: {row.get("item_id")}\n'
-#                         f'annotated by: {row.get("username")}\n'
-#                         f'Annotations: '
-#                         + ', '.join(
-#                             [f'{l.key}|{li}' for l in labels if l.options_int is not None for li in l.options_int if row[f'{l.key}|{li}'] == 1]
-#                             + [f'{l.key}|{li}' for l in labels if l.options_multi is not None for li in l.options_multi if row[f'{l.key}|{li}'] == 1]
-#                             + [f'{l.key}|{li}' for l in labels if l.options_bool is not None for li in [0, 1] if row[f'{l.key}|{li}'] == 1]
-#                         )
-#                     ],
-#                 }
-#                 for row in result
-#             ],
-#             bibliography_file,
-#         )
 
-#         return FileResponse(
-#             bibliography_file.name, background=BackgroundTask(cleanup, bibliography_file.name), media_type='application/x-research-info-systems'
-#         )
+@router.post('/annotations/ris', response_class=CFR)
+async def get_annotations_ris(
+    query: ExportRequest,
+    permissions: UserPermissions = Depends(UserPermissionChecker('annotations_read')),
+) -> FileResponse:
+    labels = query.labels
+    result = await prepare_export_table(
+        bot_annotation_metadata_ids=query.bot_annotation_metadata_ids,
+        assignment_scope_ids=query.assignment_scope_ids,
+        user_ids=query.user_ids,
+        project_id=permissions.permissions.project_id,
+        labels=labels,
+        nql_filter=query.nql_filter,
+        ignore_repeat=query.ignore_repeat,
+        ignore_hierarchy=query.ignore_hierarchy,
+        db_engine=db_engine,
+    )
+
+    with tempfile.NamedTemporaryFile(suffix='.ris', mode='w', delete=False) as bibliography_file:
+        rispy.dump(
+            [
+                {  # In prod academic_items; up to 2.5M out of 8M missing for these columns; so to make it error prone, default to empty string
+                    'abstract': row.get('text'),
+                    'title': row.get('title'),
+                    'doi': f'https://doi.org/{row.get("doi")}',
+                    'custom1': row.get('openalex_id'),
+                    'custom2': str(row.get('item_id')),
+                    'year': row.get('publication_year'),
+                    'journal_name': row.get('source'),
+                    'authors': get_author_names(row.get('authors', [])),
+                    'keywords': row.get('keywords')
+                    if row.get('keywords')
+                    else []
+                    + [f'{l.key}|{li}' for l in labels if l.options_int is not None for li in l.options_int if row[f'{l.key}|{li}'] == 1]
+                    + [f'{l.key}|{li}' for l in labels if l.options_multi is not None for li in l.options_multi if row[f'{l.key}|{li}'] == 1]
+                    + [f'{l.key}|{li}' for l in labels if l.options_bool is not None for li in [0, 1] if row[f'{l.key}|{li}'] == 1],
+                    'label': [f'{l.key}|{li}' for l in labels if l.options_int is not None for li in l.options_int if row[f'{l.key}|{li}'] == 1]
+                    + [f'{l.key}|{li}' for l in labels if l.options_multi is not None for li in l.options_multi if row[f'{l.key}|{li}'] == 1]
+                    + [f'{l.key}|{li}' for l in labels if l.options_bool is not None for li in [0, 1] if row[f'{l.key}|{li}'] == 1],
+                    'notes': [
+                        f'openalex: {row.get("openalex_id")}\n'
+                        f'nacsos: {row.get("item_id")}\n'
+                        f'annotated by: {row.get("username")}\n'
+                        f'Annotations: '
+                        + ', '.join(
+                            [f'{l.key}|{li}' for l in labels if l.options_int is not None for li in l.options_int if row[f'{l.key}|{li}'] == 1]
+                            + [f'{l.key}|{li}' for l in labels if l.options_multi is not None for li in l.options_multi if row[f'{l.key}|{li}'] == 1]
+                            + [f'{l.key}|{li}' for l in labels if l.options_bool is not None for li in [0, 1] if row[f'{l.key}|{li}'] == 1]
+                        )
+                    ],
+                }
+                for row in result
+            ],
+            bibliography_file,
+        )
+
+        return FileResponse(
+            bibliography_file.name, background=BackgroundTask(cleanup, bibliography_file.name), media_type='application/x-research-info-systems'
+        )
 
 
 class ProjectBaseInfoEntry(BaseModel):
